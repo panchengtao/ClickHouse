f28c4dd58f600624a07e2c395b4114a8eb8a4272
vdimir <vdimir@clickhouse.com>
2023-06-02 17:43:08 +0000

    upd

--- src/Storages/MergeTree/MergeTreeData.cpp
+++ src/Storages/MergeTree/MergeTreeData.cpp
@@ -7946,9 +7946,6 @@ MovePartsOutcome MergeTreeData::moveParts(const CurrentlyMovingPartsTaggerPtr & 
         catch (...)
         {
             write_part_log(ExecutionStatus::fromCurrentException("", true));
-            if (cloned_part.part)
-                cloned_part.part->remove();
-
             throw;
         }
     }
